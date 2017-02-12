55029aa84ad8ac38b36d69f92010bc018699fc6f
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-12 09:09:28 +0900

    Initial Gradle Project

---
+++ src/main/kotlin/com/masahirosaito/spigot/mscore/MSCore.kt
@@ -0,0 +1,14 @@
+package com.masahirosaito.spigot.mscore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MSCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
