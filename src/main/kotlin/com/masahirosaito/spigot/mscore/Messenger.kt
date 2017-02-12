e817dcb7539cf97b50a79ebf5d23d58620b4920b
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-12 10:29:42 +0900

    [Edit] ConsoleSender からメッセージを送信

--- src/main/kotlin/com/masahirosaito/spigot/mscore/Messenger.kt
+++ src/main/kotlin/com/masahirosaito/spigot/mscore/Messenger.kt
@@ -1,16 +1,15 @@
 package com.masahirosaito.spigot.mscore
 
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
-import java.util.logging.Logger
 
 class Messenger(val plugin: JavaPlugin, val onDebug: Boolean) {
-    val logger: Logger = plugin.logger
 
     fun prefix(obj: Any) = "[${plugin.name}] $obj"
 
-    fun log(obj: Any) = logger.info("$obj")
+    fun log(obj: Any) = Bukkit.getServer().consoleSender.sendMessage(prefix(obj))
 
     fun debug(obj: Any) {
         if (onDebug) log("${ChatColor.AQUA}[DEBUG]${ChatColor.RESET} $obj")
