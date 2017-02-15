9c56161180f9cf84b9c48ff4ebc0b6fc065ad882
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 18:41:47 +0900

    [Edit] デバッグ用のコードを削除

--- src/main/java/com/masahirosaito/spigot/mscore/UpdateChecker.kt
+++ src/main/java/com/masahirosaito/spigot/mscore/UpdateChecker.kt
@@ -10,7 +10,7 @@ class UpdateChecker(val organization: String, val repository: String, val name: 
     fun getLatestVersion(): String {
         return url.openConnection().inputStream.bufferedReader().lines()
                 .filter { it.contains(Regex("\"/$organization/$repository/$name/([\\d.]+\").*(title)")) }
-                .map { it.apply { println(it) }.replace(Regex("<.+?>"), "") }.toArray().first() as String
+                .map { it.replace(Regex("<.+?>"), "") }.toArray().first() as String
     }
 
     fun isLatestVersion(plugin: JavaPlugin): Boolean {
