6fd161666592b31a87c73bdca7552038149e5227
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 20:14:17 +0900

    [Add] PlayerUtil に関数追加

--- src/main/java/com/masahirosaito/spigot/mscore/utils/PlayerUtil.kt
+++ src/main/java/com/masahirosaito/spigot/mscore/utils/PlayerUtil.kt
@@ -3,8 +3,13 @@ package com.masahirosaito.spigot.mscore.utils
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.Sound
+import org.bukkit.Statistic
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.ExperienceOrb
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerStatisticIncrementEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
 
 fun Player.itemInMainHand() = inventory.itemInMainHand!!
 
@@ -13,4 +18,36 @@ fun Player.isCreativeMode() = gameMode == GameMode.CREATIVE
 fun Player.onBreakItemInMainHand() {
     world.playSound(location, Sound.ENTITY_ITEM_BREAK, 1f, 1f)
     inventory.itemInMainHand = ItemStack(Material.AIR)
+}
+
+fun Player.incrementGeneralStatistic(plugin: JavaPlugin, statistic: Statistic, amount: Int? = null) {
+    player.getStatistic(statistic).let {
+        PlayerStatisticIncrementEvent(this, statistic, it, it + (amount ?: 1)).call(plugin).apply {
+            if (!isCancelled) this.player.incrementStatistic(this.statistic, this.newValue - this.previousValue)
+        }
+    }
+}
+
+fun Player.incrementMaterialStatistic(plugin: JavaPlugin, statistic: Statistic, material: Material, amount: Int? = null) {
+    player.getStatistic(statistic, material).let {
+        PlayerStatisticIncrementEvent(this, statistic, it, it + (amount ?: 1), material).call(plugin).apply {
+            if (!isCancelled) this.player.incrementStatistic(this.statistic, this.material, this.newValue - this.previousValue)
+        }
+    }
+}
+
+fun Player.incrementEntityStatistic(plugin: JavaPlugin, statistic: Statistic, entityType: EntityType, amount: Int? = null) {
+    player.getStatistic(statistic, entityType).let {
+        PlayerStatisticIncrementEvent(this, statistic, it, it + (amount ?: 1), entityType).call(plugin).apply {
+            if (!isCancelled) this.player.incrementStatistic(this.statistic, this.entityType, this.newValue - this.previousValue)
+        }
+    }
+}
+
+fun Player.spawnExp(amount: Int) {
+    location.world.spawn(location, ExperienceOrb::class.java).experience = amount
+}
+
+fun Player.spawnItem(itemStack: ItemStack) {
+    location.world.dropItemNaturally(location, itemStack)
 }
