669c8dcab95ac8ee8bed91e521a273666c8a4574
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 20:26:58 +0900

    [Edit] ItemStackの残り耐久値を拡張関数から拡張プロパティに変更

--- src/main/java/com/masahirosaito/spigot/mscore/utils/ItemStackUtil.kt
+++ src/main/java/com/masahirosaito/spigot/mscore/utils/ItemStackUtil.kt
@@ -4,8 +4,8 @@ import org.bukkit.inventory.ItemStack
 
 fun ItemStack.isBreak(damage: Int) = durability + damage >= type.maxDurability
 
-fun ItemStack.getRemainingDurability() = type.maxDurability - durability
-
 fun ItemStack.damage(damage: Int) = this.apply { durability = durability.plus(damage).toShort() }
 
 fun ItemStack.isBroken() = durability >= type.maxDurability
+
+val ItemStack.remainingDurability: Int get() = type.maxDurability - durability
