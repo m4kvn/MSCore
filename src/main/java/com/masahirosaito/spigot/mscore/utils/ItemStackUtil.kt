47b14e323e0fdd080a5618c592627b5d6bc242bc
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 20:29:07 +0900

    [Edit] 拡張関数から拡張プロパティに変更

--- src/main/java/com/masahirosaito/spigot/mscore/utils/ItemStackUtil.kt
+++ src/main/java/com/masahirosaito/spigot/mscore/utils/ItemStackUtil.kt
@@ -6,6 +6,6 @@ fun ItemStack.isBreak(damage: Int) = durability + damage >= type.maxDurability
 
 fun ItemStack.damage(damage: Int) = this.apply { durability = durability.plus(damage).toShort() }
 
-fun ItemStack.isBroken() = durability >= type.maxDurability
-
 val ItemStack.remainingDurability: Int get() = type.maxDurability - durability
+
+val ItemStack.isBroken: Boolean get() = durability >= type.maxDurability
