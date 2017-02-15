c3142fa173836ac5e6ad57026a276ed4d01688df
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 18:33:13 +0900

    [Add] プラグインのバージョンが最新か確認するメソッド

--- src/main/java/com/masahirosaito/spigot/mscore/UpdateChecker.kt
+++ src/main/java/com/masahirosaito/spigot/mscore/UpdateChecker.kt
@@ -1,5 +1,6 @@
 package com.masahirosaito.spigot.mscore
 
+import org.bukkit.plugin.java.JavaPlugin
 import java.net.URL
 
 class UpdateChecker(val organization: String, val repository: String, val name: String) {
@@ -10,5 +11,13 @@ class UpdateChecker(val organization: String, val repository: String, val name: 
         return url.openConnection().inputStream.bufferedReader().lines()
                 .filter { it.contains(Regex("\"/$organization/$repository/$name/([\\d.]+\").*(title)")) }
                 .map { it.apply { println(it) }.replace(Regex("<.+?>"), "") }.toArray().first() as String
+    }
+
+    fun isLatestVersion(plugin: JavaPlugin): Boolean {
+        try {
+            return plugin.description.version == getLatestVersion()
+        } catch(e: Exception) {
+            return true
+        }
     }
 }
