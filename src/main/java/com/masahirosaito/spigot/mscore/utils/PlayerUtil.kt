5cd0a1cc0e7ed22fcaad7e39c38d37addc2b1657
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 20:21:41 +0900

    [Remove] 必要ないと思ったメソッドの削除

--- src/main/java/com/masahirosaito/spigot/mscore/utils/PlayerUtil.kt
+++ src/main/java/com/masahirosaito/spigot/mscore/utils/PlayerUtil.kt
@@ -20,34 +20,6 @@ fun Player.onBreakItemInMainHand() {
     inventory.itemInMainHand = ItemStack(Material.AIR)
 }
 
-fun Player.incrementGeneralStatistic(plugin: JavaPlugin, statistic: Statistic, amount: Int? = null) {
-    player.getStatistic(statistic).let {
-        PlayerStatisticIncrementEvent(this, statistic, it, it + (amount ?: 1)).call(plugin).apply {
-            if (!isCancelled) this.player.incrementStatistic(this.statistic, this.newValue - this.previousValue)
-        }
-    }
-}
-
-fun Player.incrementMaterialStatistic(plugin: JavaPlugin, statistic: Statistic, material: Material, amount: Int? = null) {
-    player.getStatistic(statistic, material).let {
-        PlayerStatisticIncrementEvent(this, statistic, it, it + (amount ?: 1), material).call(plugin).apply {
-            if (!isCancelled) this.player.incrementStatistic(this.statistic, this.material, this.newValue - this.previousValue)
-        }
-    }
-}
-
-fun Player.incrementEntityStatistic(plugin: JavaPlugin, statistic: Statistic, entityType: EntityType, amount: Int? = null) {
-    player.getStatistic(statistic, entityType).let {
-        PlayerStatisticIncrementEvent(this, statistic, it, it + (amount ?: 1), entityType).call(plugin).apply {
-            if (!isCancelled) this.player.incrementStatistic(this.statistic, this.entityType, this.newValue - this.previousValue)
-        }
-    }
-}
-
 fun Player.spawnExp(amount: Int) {
     location.world.spawn(location, ExperienceOrb::class.java).experience = amount
-}
-
-fun Player.spawnItem(itemStack: ItemStack) {
-    location.world.dropItemNaturally(location, itemStack)
 }
